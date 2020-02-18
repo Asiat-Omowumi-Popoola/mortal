a6aae8b3a1658e6d24558e1bc43cfa37f429b017
Asiat-Omowumi-Popoola <61153287+Asiat-Omowumi-Popoola@users.noreply.github.com>
2020-02-18 13:19:23 +0100

    first commit

---
+++ mortal.pl
@@ -0,0 +1,12 @@
+% This is the syntax for comments.
+% MORTAL - The first illustrative Prolog program
+
+mortal(X) :- person(X).
+preson(socrates).
+person(plato).
+person(aristotle).
+
+mortal_report :- write('Known mortals are:'),nl,
+ mortal(X),
+ write(X),nl,
+ fail.
